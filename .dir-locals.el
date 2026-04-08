619bdadc4ac5f834001390c1da2971e9f974f966
Steve Downey <sdowney@gmail.com>
2026-04-08 14:29:00 +0100

    Squashed 'checkdoc-batch/' content from commit 167cef9
    git-subtree-dir: checkdoc-batch
    git-subtree-split: 167cef920e61f989a535d31ee5f1d825ed530427

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+((nil . ((ispell-local-dictionary . "american")
+         (sentence-end-double-space . t)
+         (fill-column . 79)
+         (eval . (when (and (boundp 'projectile-project-types)
+                            (alist-get 'pk/emacs-package projectile-project-types))
+                   (setq projectile-project-type 'pk/emacs-package)))))
+ (emacs-lisp-mode . ((indent-tabs-mode . nil)
+                     (flycheck-emacs-lisp-load-path . inherit))))
---
+++ Cask
@@ -0,0 +1,8 @@
+(source gnu)
+(source melpa)
+
+(package-file "checkdoc-batch.el")
+
+(development
+ (depends-on "package-lint")
+ (depends-on "relint"))
