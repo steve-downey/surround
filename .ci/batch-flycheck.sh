c2d9107b7eb819c6f0bb92cb3bef7ce078b956e1
Steve Downey <sdowney@gmail.com>
2026-04-08 14:40:09 +0100

    Batch flycheck
    Flycheck everything

---
+++ .ci/batch-flycheck.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+
+EMACS=${EMACS:=emacs}
+PACKAGE_DIR=.
+MINIMAL_DIR=.minimal-emacs.d
+
+# Use find to find file names such that globs are expanded while prevent
+# splitting paths on spaces
+mapfile -t files <<< \
+        "$(ls *.el)"
+
+${EMACS} -Q --batch \
+         --init-dir=./ \
+         --eval '
+(progn
+  (setq debug-on-error t
+        eval-expression-print-length 100
+        edebug-print-length 500
+        treesit-auto-install nil)
+  (load-file "'"${MINIMAL_DIR}"'/early-init.el")
+  (load-file "'"${MINIMAL_DIR}"'/init.el")
+  (use-package flycheck :ensure t)
+  (load-file "'"${PACKAGE_DIR}"'/.ci/batch-flycheck.el"))' \
+         --funcall batch-flycheck \
+         "${files[@]}"
